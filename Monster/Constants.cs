756f385e62fe6f8ec50091c0878a2cfe3fa9a5ed
Andrew <21085039+asmarsh@users.noreply.github.com>
2021-05-10 20:10:39 -0400

    Constants updated for new monster types

--- Monster/Constants.cs
+++ Monster/Constants.cs
@@ -16,5 +16,31 @@ namespace Monster
             public const string TOSTRING = "I am a Ghost!";
         }
 
+        public class Vampire
+        {
+            public const string ATTACK = "You have been bitten!";
+            public const string SCARE = "I am a vampire!";
+            public const string SECONDARYATTACK = "You have been compelled to do my bidding!";
+            public const string DEFEND = "Turn into a bat and fly away...";
+            public const string TOSTRING = "I am a Vampire!";
+        }
+
+        public class Zombie
+        {
+            public const string ATTACK = "You have been bitten!";
+            public const string SCARE = "Scream!";
+            public const string DEFEND = "";
+            public const string TOSTRING = "I am a Zombie!";
+        }
+
+        public class Werewolf
+        {
+            public const string ATTACK = "Bite!";
+            public const string SCARE = "Growl!";
+            public const string DEFEND = "Run away!";
+            public const string SECONDARYATTACK = "Claw!";
+            public const string TOSTRING = "I am a Lycanthrope!";
+        }
+
     }
 }
